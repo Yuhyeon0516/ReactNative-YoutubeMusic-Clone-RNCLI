e6a6dec1d2401db5ceda4009e5dcda822b5a80c1
Yuhyeon0516 <rladbgus0516@naver.com>
2023-08-16 18:42:49 +0900

    first commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+
+function App(): JSX.Element {
+  return <></>;
+}
+
+export default App;
