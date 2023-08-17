80c6a4085020f2a89f33447a2c2b309e5bf09fc9
Yuhyeon0516 <rladbgus0516@naver.com>
2023-08-17 13:48:43 +0900

    created CategoryHeader

--- src/YoutubeMusic.tsx
+++ src/YoutubeMusic.tsx
@@ -1,17 +1,23 @@
 import {View, Text, ScrollView} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import LogoHeader from './components/Header/LogoHeader';
 import HeaderCategory from './components/Header/CategoryHeader';
 import HeaderBackground from './components/Header/HeaderBackground';
 import Footer from './components/Footer/Footer';
 
 export default function YoutubeMusic() {
+  const [selectedCategory, setSelectedCategory] = useState<
+    number | undefined
+  >();
+
   return (
     <View style={{flex: 1}}>
       <HeaderBackground />
       <LogoHeader />
-      <HeaderCategory />
-
+      <HeaderCategory
+        selectedCategory={selectedCategory}
+        setSelectedCategory={setSelectedCategory}
+      />
       <ScrollView style={{borderWidth: 1}}>
         <View style={{height: 10000}}>
           <Text>Music list</Text>
--- src/components/Header/CategoryHeader.tsx
+++ src/components/Header/CategoryHeader.tsx
@@ -1,10 +1,55 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, ScrollView, TouchableOpacity} from 'react-native';
+import React, {Dispatch, SetStateAction} from 'react';
 
-export default function CategoryHeader() {
+type CategoryHeaderPropsType = {
+  selectedCategory: number | undefined;
+  setSelectedCategory: Dispatch<SetStateAction<number | undefined>>;
+};
+
+export default function CategoryHeader({
+  selectedCategory,
+  setSelectedCategory,
+}: CategoryHeaderPropsType) {
+  const category = ['휴식', '에너지 충전', '집중', '운동', '출퇴근/등하교'];
+
+  function onPressCategory(index: number) {
+    const data = selectedCategory === index ? undefined : index;
+    setSelectedCategory(data);
+  }
+
   return (
     <View>
-      <Text>CategoryHeader</Text>
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        style={{flexDirection: 'row', paddingVertical: 20}}
+        contentContainerStyle={{paddingHorizontal: 10}}>
+        {category.map((item, index) => {
+          return (
+            <TouchableOpacity onPress={() => onPressCategory(index)}>
+              <View
+                key={index}
+                style={{
+                  padding: 8,
+                  paddingHorizontal: 16,
+                  borderWidth: 0.5,
+                  borderColor: '#ffffff30',
+                  backgroundColor:
+                    selectedCategory === index ? '#fff' : '#ffffff10',
+                  marginHorizontal: 4,
+                  borderRadius: 8,
+                }}>
+                <Text
+                  style={{
+                    color: selectedCategory === index ? 'black' : 'white',
+                  }}>
+                  {item}
+                </Text>
+              </View>
+            </TouchableOpacity>
+          );
+        })}
+      </ScrollView>
     </View>
   );
 }
